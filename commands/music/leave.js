3214a4292ff00b0ef68ce6526751981a59dbc783
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-17 05:42:23 +0100

    Add files via upload

---
+++ commands/music/leave.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class LeaveCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'leave',
+      aliases: ['end'],
+      group: 'music',
+      memberName: 'leave',
+      guildOnly: true,
+      description: 'Leaves voice channel if in one'
+    });
+  }
+
+  run(message) {
+        if (message.channel.id === '718208603605172315') {
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('Join a channel and try again');
+
+    if (
+      typeof message.guild.musicData.songDispatcher == 'undefined' ||
+      message.guild.musicData.songDispatcher == null
+    ) {
+      return message.reply('There is no song playing right now!');
+    }
+    if (!message.guild.musicData.queue)
+      return message.say('There are no songs in queue');
+    message.guild.musicData.songDispatcher.end();
+    message.guild.musicData.queue.length = 0;
+    return;
+  }
+  }
+};
+ //made by CTK WARRIOR
