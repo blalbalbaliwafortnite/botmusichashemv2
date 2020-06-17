89df10465d6ecfbc8c33da5d20dc3b48c4090946
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-17 05:41:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token, discord_owner_id } = require('./config.json');
+
+Structures.extend('Guild', function(Guild) {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        volume: 1
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: discord_owner_id // value comes from config.json
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Command Group'],
+    ['gifs', 'Gif Command Group'],
+    ['other', 'random types of commands group'],
+    ['guild', 'guild related commands']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    eval: false,
+    prefix: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('voiceStateUpdate', async (___, newState) => {
+  if (
+    newState.member.user.bot &&
+    !newState.channelID &&
+    newState.guild.musicData.songDispatcher &&
+    newState.member.user.id == client.user.id
+  ) {
+    newState.guild.musicData.queue.length = 0;
+    newState.guild.musicData.songDispatcher.end();
+  }
+});
+
+const GUILDID = '716644088748703856'; //  تعديل مهم ايدي السيرفر  
+const CHANNELID = '716628968887025724'; // تعديل مهم ايدي الروم الصوتي
+
+/*client.on('ready',async () => {
+  setInterval(() => {
+client.channels.cache.find(ch => ch.id === CHANNELID && ch.type === 'voice').join();
+},1000); 
+ voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.cache.get(guildid);
+    let channel = guild.channels.cache.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type ::  ${channel.type}, It must be Voice.`);
+    }
+  }
+});*/
+
+
+client.login(token);
